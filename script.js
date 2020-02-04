4ceae270c4da27b675266c52a362e68cc754266d
Lyuben Tenekedzhiev <lyuben.tenekedjiev@gmail.com>
2020-02-04 14:31:32 +0100

    train timetable

---
+++ script.js
@@ -0,0 +1,38 @@
+
+const departures = [ 
+  {time: {hrs: 8, mins: 14}, train: 'Acela Express', no: 2153, to: 'Washington, DC', status: '', track: 2},
+  {time: {hrs: 8, mins: 19}, train: 'Acela Express', no: 2153, to: 'Washington, DC', status: '', track: 2},
+  {time: {hrs: 8, mins: 20}, train: 'Acela Express', no: 2153, to: 'Washington, DC', status: '', track: 2},
+  {time: {hrs: 8, mins: 37}, train: 'Acela Express', no: 2153, to: 'Washington, DC', status: '', track: 2},
+  {time: {hrs: 8, mins: 40}, train: 'Acela Express', no: 2153, to: 'Washington, DC', status: '', track: 2},
+  {time: {hrs: 8, mins: 43}, train: 'Acela Express', no: 2153, to: 'Washington, DC', status: '', track: 2},
+];
+
+  const trainTime = document.querySelector('.board__table');
+  for(let i = 0; i < departures.length; i++) {
+    const trainDOM = document.createElement('tr');
+    trainDOM.innerHTML = 
+    `
+    <td>${departures[i].time.hrs}:${departures[i].time.mins}</td>
+    <td>${departures[i].train}</td>
+    <td>${departures[i].no}</td>
+    <td>${departures[i].to}</td>
+    <td class='delay'>${departures[i].status}</td>
+    <td>${departures[i].track}</td>
+    <button class="isDelayed">Check if delayed</button>
+    `;
+    
+    const delay = trainDOM.querySelector('.delay')
+    const btn = trainDOM.querySelector('.isDelayed');
+    btn.addEventListener('click', () => {
+      if(delay.innerHTML === 'delayed') {
+        delay.innerHTML = 'on time'
+        trainDOM.style.backgroundColor = '';
+      } else {
+        delay.innerHTML = 'delayed';
+        trainDOM.style.backgroundColor = 'salmon';
+      }
+    });
+
+    trainTime.appendChild(trainDOM);
+  }
